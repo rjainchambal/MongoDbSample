e24355fe27564a97bd4c4d4675fe1d294903f9e3
cloudcomputingexperts <rjain@chambal.com>
2015-03-04 12:31:18 +0530

    Create new project
    Create new project with MVC4 and MongoDB and perform basic operations on
    database.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System.Web.Configuration;
+using System.Web.Mvc;
+using MongoDbSample.Models;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+
+namespace MongoDbSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            MongoDbOpeartions();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        static void MongoDbOpeartions()
+        {
+            string uri = WebConfigurationManager.ConnectionStrings["MongoDbUri"].ConnectionString;
+            //const string uri = "mongodb://admin:admin@ds031277.mongolab.com:31277/sampledb";
+            MongoUrl url = new MongoUrl(uri);
+            MongoClient client = new MongoClient(url);
+            MongoServer server = client.GetServer();
+            MongoDatabase db = server.GetDatabase("sampledb");
+
+            var collectionCustomer = db.GetCollection<Customer>("customer");
+
+            //BsonDocument[] seedData = CreateSeedData();
+            //customer.InsertBatch(seedData);
+
+            //insert
+            Customer customer1 = new Customer { Name = "Apurva Jain", Address = "Udaipur", Country = "India", Phone = "999999999" };
+            collectionCustomer.Insert(customer1);
+            
+            Customer customer2 = new Customer { Name = "Ronak Jain",Address = "Rishabhdeo", Country = "India", Phone = "9887594812"};
+            collectionCustomer.Insert(customer2);
+            var id = customer2.Id;
+
+
+            //find
+            var query = Query<Customer>.EQ(e => e.Id, id);
+            Customer cust = collectionCustomer.FindOne(query);
+   
+            //save
+            cust.Address = "Rishabhdeo, Udaipur, Rajasthan";
+            collectionCustomer.Save(cust);
+
+            //update
+            var update = Update<Customer>.Set(e => e.Name, "Ronak Kumar Jain");
+            collectionCustomer.Update(query, update);
+
+            //remove
+            collectionCustomer.Remove(query);
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+
+namespace MongoDbSample.Models
+{
+    public class Customer
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public String Address { get; set; }
+        public String Phone { get; set; }
+        public String Country { get; set; }
+    }
+}
